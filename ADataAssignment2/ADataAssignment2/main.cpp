4353fd10d322b3c6045d54ee069d6fa4f65341ae
Kasterborous420 <yuckar97@gmail.com>
2016-07-21 02:34:19 +0800

    I did things.
    I did things.

--- ADataAssignment2/ADataAssignment2/main.cpp
+++ ADataAssignment2/ADataAssignment2/main.cpp
@@ -10,13 +10,42 @@ using std::getline;
 using std::ofstream;
 
 //File To Read//
-ofstream FileToRead;
+std::ifstream FileToRead;
 
-void ReadCSV(ofstream FileToRead)
+
+void ReadCSV()
 {
-	FileToRead.open("Cards.csv");
+	FileToRead.open("CR_Cards.csv");
+	string line;
+	int size = 0;
+
+	if (FileToRead.is_open())
+	{
+		while (getline(FileToRead, line))
+		{
+			size++;
+		}
+		CCards *cardArray = new CCards[size];
+
+		while (getline(FileToRead, line))
+		{
+			int index = 0;
+
+			CCards card;
+
+			//Read each string to ","
+			//how to check for the end though?
+
+
+			cardArray[index] = card;
+			index++;
+
+		}
+		FileToRead.close();
+	}
 }
 void main()
 {
-
+	ReadCSV();
+	system("pause");
 }
